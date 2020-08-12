e15905b3d186c3d5d5847dc258db939afee13ab3
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-08-12 13:10:11 +0300

    Initial Commit

---
+++ DisclosureGroup/DisclosureGroupApp.swift
@@ -0,0 +1,17 @@
+//
+//  DisclosureGroupApp.swift
+//  DisclosureGroup
+//
+//  Created by ramil on 12.08.2020.
+//
+
+import SwiftUI
+
+@main
+struct DisclosureGroupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
